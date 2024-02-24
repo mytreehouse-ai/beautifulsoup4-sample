c85f9331cfbea7161e269839b04372ee30599488
casulit <chrisgelosulit@gmail.com>
2024-02-24 12:26:22 +0800

    chore(vscode): add .vscode/settings for better dev environmentstyle: improve code formatting and whitespace in Python script

--- lamudi_single_property.py
+++ lamudi_single_property.py
@@ -1,12 +1,13 @@
 import json
 from bs4 import BeautifulSoup
 
+
 def extract_property_details(jsonl_file):
     with open(jsonl_file, 'r') as file:
         # Reading line by line (assuming each line is a separate JSON object)
         for line in file:
             json_data = json.loads(line)
-            
+
             # Assuming 'html' is the correct key holding the HTML content
             html_content = json_data['result']
             soup = BeautifulSoup(html_content, 'html.parser')
@@ -21,14 +22,27 @@ def extract_property_details(jsonl_file):
 
             return property_details
 
+
 def extract_address(soup):
     address = soup.find('span', {'class': 'Title-pdp-address'})
     return address.text if address else 'n/a'
 
+
 def extract_description(soup):
-    description_div = soup.find('div', {'class': 'listing-description ViewMore js-ViewMoreSection initialized'})
-    description_text = description_div.find('div', {'class': 'ViewMore-text-description'}) if description_div else None
+    description_div = soup.find(
+        'div',
+        {
+            'class': 'listing-description ViewMore js-ViewMoreSection initialized'
+        }
+    )
+    description_text = description_div.find(
+        'div',
+        {
+            'class': 'ViewMore-text-description'
+        }
+    ) if description_div else None
     return description_text.text if description_text else 'n/a'
+
 
 def extract_images(soup):
     images = []
@@ -40,9 +54,11 @@ def extract_images(soup):
                 images.append(data_src)
     return images
 
+
 def extract_amenities(soup):
     amenities_div = soup.find('div', {'id': 'listing-amenities'})
     return amenities_div.text.split() if amenities_div else []
+
 
 # Provide the path to the JSONL file
 jsonl_file = 'jsonl/warehouse-single-2-job-848553-result.jsonl'
